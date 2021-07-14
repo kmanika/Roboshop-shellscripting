ee4deeb3274a5816e912749d349f0a249df103e6
KalluruS <>
2021-07-14 08:05:57 +0530

    Update shell-scripting/

--- 03-vars.sh
+++ 03-vars.sh
@@ -3,9 +3,12 @@
 A=10
 echo A = $A
 DATE="2021-07-14"
-echo Welcome, Today date is $DATE 
+echo Welcome, Today date is $DATE
 
 ## Command Subs
 NO_OF_USERS=$(who |wc -l)
 echo Number of Users = $NO_OF_USERS
 
+## Date with command substitution
+DATE=$(date +%F)
+echo Welcome, Today date is $DATE
