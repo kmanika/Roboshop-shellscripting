f78a6deb1361f7488b8bc51394911d551067433c
KalluruS <>
2021-07-16 08:34:19 +0530

    Update shell-scripting/

--- roboshop/frontend.sh
+++ roboshop/frontend.sh
@@ -13,7 +13,7 @@ fi
 
 ### 3. Need to validate whether the script is running as root user not.
 
-echo -n -e "Enabling Nginx\t\t..."
+echo -n -e "Enabling Nginx\t\t\t..."
 systemctl enable nginx  &>>$LOG
 if [ $? -eq 0 ]; then
   echo -e "\e[32m done\e[0m"
@@ -21,7 +21,7 @@ else
   echo fail
 fi
 
-echo -n -e "Starting Nginx\t\t..."
+echo -n -e "Starting Nginx\t\t\t..."
 systemctl start nginx  &>>$LOG
 if [ $? -eq 0 ]; then
   echo -e "\e[32m done\e[0m"
