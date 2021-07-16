e358820d8c8f4f4beae7fefbbedb4d3780d8dfbc
KalluruS <>
2021-07-16 07:48:01 +0530

    Update shell-scripting/

--- roboshop/frontend.sh
+++ roboshop/frontend.sh
@@ -2,7 +2,7 @@
 
 LOG=/tmp/roboshop.log
 
-echo -e "Installing Nginx\t\t...\t\e[32mdone\e[0m"
+echo -e "Installing Nginx\t\t... \e[32mdone\e[0m"
 yum install nginx -y >$LOG
 ### 1. Output from commands should not be displayed on screen
 ### 2. Validate the command is successful or not
