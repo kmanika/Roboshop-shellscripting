c3c930f3c854f8f46c8c112da89b6db62109541b
KalluruS <>
2021-07-16 08:38:21 +0530

    Update shell-scripting/

--- roboshop/frontend.sh
+++ roboshop/frontend.sh
@@ -3,33 +3,26 @@
 LOG=/tmp/roboshop.log
 rm -f $LOG
 
+STAT_CHECK() {
+  if [ $1 -eq 0 ]; then
+    echo -e "\e[32m done\e[0m"
+  else
+    echo -e "\e[31m fail\e[0m"
+    exit 1
+  fi
+}
+
 echo -n -e "Installing Nginx\t\t..."
 yumm install nginx -y &>>$LOG
-if [ $? -eq 0 ]; then
-  echo -e "\e[32m done\e[0m"
-else
-  echo -e "\e[31m fail\e[0m"
-  exit 1
-fi
+STAT_CHECK $?
 
 ### 3. Need to validate whether the script is running as root user not.
 
 echo -n -e "Enabling Nginx\t\t\t..."
 systemctl enable nginx  &>>$LOG
-if [ $? -eq 0 ]; then
-  echo -e "\e[32m done\e[0m"
-else
-  echo -e "\e[31m fail\e[0m"
-  exit 1
-fi
+STAT_CHECK $?
 
 echo -n -e "Starting Nginx\t\t\t..."
 systemctl start nginx  &>>$LOG
-if [ $? -eq 0 ]; then
-  echo -e "\e[32m done\e[0m"
-else
-  echo -e "\e[31m fail\e[0m"
-  exit 1
-fi
-
+STAT_CHECK $?
 
