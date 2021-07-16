a69d197727f9f8dae93bdc7c2c30d5b1d79399d5
KalluruS <>
2021-07-16 08:19:06 +0530

    Update shell-scripting/

--- roboshop/frontend.sh
+++ roboshop/frontend.sh
@@ -5,13 +5,17 @@ rm -f $LOG
 
 echo -e "Installing Nginx\t\t... \e[32mdone\e[0m"
 yum install nginx -y &>>$LOG
+echo $?
 ### 1. Output from commands should not be displayed on screen
 ### 2. Validate the command is successful or not
 ### 3. Need to validate whether the script is running as root user not.
 
 echo "Enabling Nginx"
 systemctl enable nginx  &>>$LOG
+echo $?
 
 echo "Starting Nginx"
 systemctl start nginx  &>>$LOG
+echo $?
+
 
