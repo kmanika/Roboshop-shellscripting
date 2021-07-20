c86b1dc48deb41fb9bafe7d01873bc533a3894aa
KalluruS <>
2021-07-20 08:16:59 +0530

    Update shell-scripting/

--- roboshop/catalogue.sh
+++ roboshop/catalogue.sh
@@ -10,16 +10,16 @@ PRINT "Add RoboShop Application User"
 id roboshop &>>$LOG
 if [ $? -ne 0 ]; then
   useradd roboshop &>>$LOG
-fi 
+fi
 STAT_CHECK $?
 
+PRINT "Download Application Code"
+curl -s -L -o /tmp/catalogue.zip "https://github.com/roboshop-devops-project/catalogue/archive/main.zip" &>>$LOG
+STAT_CHECK $?
 
-#$ curl -s -L -o /tmp/catalogue.zip "https://github.com/roboshop-devops-project/catalogue/archive/main.zip"
-#$ cd /home/roboshop
-#$ unzip /tmp/catalogue.zip
-#$ mv catalogue-main catalogue
-#$ cd /home/roboshop/catalogue
-#$ npm install
+PRINT "Extract Downlaoded Code"
+cd /home/roboshop && unzip -o  /tmp/catalogue.zip &>>$LOG  && mv catalogue-main catalogue && cd /home/roboshop/catalogue && npm install
+STAT_CHECK $?
 
 # mv /home/roboshop/catalogue/systemd.service /etc/systemd/system/catalogue.service
 # systemctl daemon-reload
