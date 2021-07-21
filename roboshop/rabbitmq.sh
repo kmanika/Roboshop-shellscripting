d37473992523f495c239091ae94003643bdedceb
KalluruS <>
2021-07-21 08:58:12 +0530

    Update shell-scripting/

--- roboshop/rabbitmq.sh
+++ roboshop/rabbitmq.sh
@@ -22,7 +22,10 @@ systemctl enable rabbitmq-server &>>$LOG && systemctl start rabbitmq-server &>>$
 STAT_CHECK $?
 
 PRINT "Create App User in RabbitMQ"
-rabbitmqctl add_user roboshop roboshop123
-STAT_CHECK $? 
-# rabbitmqctl set_user_tags roboshop administrator
-# rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*"
+rabbitmqctl  list_users | grep roboshop &>>$LOG
+if [ $? -ne 0 ]; then
+  rabbitmqctl add_user roboshop roboshop123 &>>$LOG
+fi
+rabbitmqctl set_user_tags roboshop administrator &>>$LOG  && rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*" &>>$LOG
+STAT_CHECK $?
+
