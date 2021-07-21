31287d847137eaa30799209bac1bb36630f0b866
KalluruS <>
2021-07-21 08:56:20 +0530

    Update shell-scripting/

--- roboshop/rabbitmq.sh
+++ roboshop/rabbitmq.sh
@@ -6,16 +6,23 @@ PRINT "Install ERLang"
 yum list installed | grep erlang &>>$LOG
 if [ $? -ne 0 ]; then
   yum install https://github.com/rabbitmq/erlang-rpm/releases/download/v23.2.6/erlang-23.2.6-1.el7.x86_64.rpm -y  &>>$LOG
-fi 
+fi
 STAT_CHECK $?
 
-# curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | sudo bash
+PRINT "Setup RabbitMQ repos"
+curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | sudo bash &>>$LOG
+STAT_CHECK $?
 
-# yum install rabbitmq-server -y
+PRINT "Install RabbitMQ Server"
+yum install rabbitmq-server -y &>>$LOG
+STAT_CHECK $?
 
-# systemctl enable rabbitmq-server
-# systemctl start rabbitmq-server
+PRINT "Start RabbitMQ Service"
+systemctl enable rabbitmq-server &>>$LOG && systemctl start rabbitmq-server &>>$LOG
+STAT_CHECK $?
 
-# rabbitmqctl add_user roboshop roboshop123
+PRINT "Create App User in RabbitMQ"
+rabbitmqctl add_user roboshop roboshop123
+STAT_CHECK $? 
 # rabbitmqctl set_user_tags roboshop administrator
 # rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*"
