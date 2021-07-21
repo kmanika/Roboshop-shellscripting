95c433b14aebf3914f9bafba5deb7540ebd486e5
KalluruS <>
2021-07-21 08:13:04 +0530

    Update shell-scripting/

--- roboshop/mysql.sh
+++ roboshop/mysql.sh
@@ -4,10 +4,10 @@ source common.sh
 
 PRINT "Setup MySQL Repos"
 echo '[mysql57-community]
-#name=MySQL 5.7 Community Server
-#baseurl=http://repo.mysql.com/yum/mysql-5.7-community/el/7/$basearch/
-#enabled=1
-#gpgcheck=0' > /etc/yum.repos.d/mysql.repo
+name=MySQL 5.7 Community Server
+baseurl=http://repo.mysql.com/yum/mysql-5.7-community/el/7/$basearch/
+enabled=1
+gpgcheck=0' > /etc/yum.repos.d/mysql.repo
 STAT_CHECK $?
 
 
